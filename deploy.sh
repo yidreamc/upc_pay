1e7514500af0bbfce7e214386d26ed8690a7d50a
yidreamc <yidreamc@gmail.com>
2018-07-31 20:08:55 +0800

    前后端分离

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 echo 'deploy front'
-cd payfront
+cd front
 npm run build
 cd dist
 scp index.html root@wxsportscard.upc.edu.cn:/home/wwwroot/default
 scp -r static root@wxsportscard.upc.edu.cn:/home/wwwroot/default
 echo 'deploy server'
-cd ../../payng
+cd ../../server
 ./mvnw.cmd package
 cd target
 scp payng-0.0.1-SNAPSHOT.jar root@wxsportscard.upc.edu.cn:/root/payng

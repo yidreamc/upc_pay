a85375346c55cb17e7b313f8d89398e36e8f7052
yidreamc <yidreamc@gmail.com>
2018-10-24 16:31:27 +0800

    更新readme

--- app/src/main/java/ml/yidreamc/upcpay/notice/intercepter/NoticeIntercepter.java
+++ app/src/main/java/ml/yidreamc/upcpay/notice/intercepter/NoticeIntercepter.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
